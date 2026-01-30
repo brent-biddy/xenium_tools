31798279bb949fc75e86a912111391010e561160
Brent <255204883+brent-biddy@users.noreply.github.com>
2026-01-30 12:48:37 -0600

    Refactored create_seurat script. Script now adds xenium pca to Seurat Object.

--- bin/create_seurat_xenium.R
+++ bin/create_seurat_xenium.R
@@ -1,86 +1,114 @@
 #!/usr/bin/env Rscript
 
-library(Seurat, quietly=TRUE)
-library(data.table, quietly=TRUE)
-library(dplyr, quietly=TRUE)
+library(Seurat, quietly = TRUE)
+library(data.table, quietly = TRUE)
+library(dplyr, quietly = TRUE)
+library(argparse, quietly = TRUE)
+library(SeuratObject, quietly = TRUE)
+
 
 arguments <- function(){
     
-    library(argparse)
-
     parser <- ArgumentParser(description='Create a Seurat object from Xenium data')
     parser$add_argument('--data_dir', required=TRUE, help='Path to the Xenium data directory')
     parser$add_argument('--sample_name', required=FALSE, help='Sample name for the Seurat object', default='Xenium_Sample')
     parser$add_argument('--downsample', required = FALSE, action='store_true', help='Whether to downsample the data')
+
     args <- parser$parse_args()
+    
     return(args)    
 }
 
-create_seurat_xenium <- function(data_dir, sample_name = "Xenium_Sample"){
+add_meta_and_clusters <- function(seurat_obj, data_dir){
 
-    # output_file <- file.path(data_dir, paste0(sample_name, "_seurat.RDS"))
-    output_file <- paste0(sample_name, "_seurat.RDS")
-    cluster_file <- file.path(data_dir, "analysis", "clustering", "gene_expression_graphclust", "clusters.csv")
-    umap_file <- file.path(data_dir, "analysis", "umap", "gene_expression_2_components", "projection.csv")
+    print("Adding Cell Meta Data and Cluster Information to Seurat Object")
     cell_meta_file <- file.path(data_dir, "cells.csv.gz")
-
-    print(paste("Creating Seurat object from Xenium data in: ", data_dir))
-    print(paste("Output will be saved to: ", output_file))
-    print(paste("Sample name set to: ", sample_name))
-
-    # Load Xenium data
-    seurat_obj <- LoadXenium(data.dir = data_dir, fov = "fov", segmentations = "cell")
-    seurat_obj[["cell_id"]] <- rownames(seurat_obj[[]])
-    seurat_obj$orig.ident <- sample_name
-   
+    cluster_file <- file.path(data_dir, "analysis", "clustering", "gene_expression_graphclust", "clusters.csv")
+    
     if(file.exists(cluster_file)){
+
         print("Clustering data found. Adding Clustering Data to Seurat object.")
-        print(paste0("Cluster data from the following file will be used: ", cluster_file))
+        print(paste("Cluster data from the following file will be used: ", cluster_file))
         cluster_dat <- fread(cluster_file, data.table=FALSE, col.names=c("cell_id", "xenium_clusters"), colClasses=c("character", "integer"))
         seurat_obj[[]] <- left_join(seurat_obj[[]], cluster_dat, by="cell_id")
     }
 
     if(file.exists(cell_meta_file)){
+
         print("Cell Meta Data found. Adding meta data to Seurat object.")
-        print(paste0("Meta data from the following file will be used: ", cell_meta_file))
+        print(paste("Meta data from the following file will be used: ", cell_meta_file))
         cell_meta_dat <- fread(cell_meta_file, data.table=FALSE, colClasses = c("cell_id"="character"))
         seurat_obj[[]] <- left_join(seurat_obj[[]][, !(colnames(seurat_obj[[]]) %in% c("segmentation_method"))], cell_meta_dat, by="cell_id")
     }
 
-    if(file.exists(umap_file)){
-        print("UMAP Projection found. Adding UMAP Projection to Seurat Object.")
-        print(paste0("Projection from the following file will be used: ", umap_file))
-        umap_dat <- fread(umap_file, col.names = c("Barcode", "UMAP_1", "UMAP_2"), data.table=FALSE)
-        row.names(umap_dat) <- umap_dat[["Barcode"]]
-        seurat_obj@reductions$xenium_umap <- SeuratObject::CreateDimReducObject(embeddings = as.matrix(umap_dat[c("UMAP_1", "UMAP_2")]), key = "UMAP_", assay = "Xenium")
-    }
-    
-    if( file.exists(output_file) ){
-        # output_file <- file.path(data_dir, paste0(sample_name, "_seurat_", as.integer(Sys.time()), ".RDS"))
-        output_file <- paste0(sample_name, "_seurat_", as.integer(Sys.time()), ".RDS")
-    }
-
-    # Save Seurat object
     Idents(seurat_obj) <- "xenium_clusters"
-    saveRDS(seurat_obj, file = "seurat_object.RDS")
+
     return(seurat_obj)
+
 }
 
-downsample_seurat_xenium <- function(seurat_obj, data_dir, sample_name="Xenium_Sample"){
+add_reductions <- function(seurat_obj, data_dir){
     
-    output_file <- file.path(data_dir, paste0(sample_name, "_seurat_downsampled.RDS"))
-
-    print("Downsampling Seurat object")
-    print(paste0("Downsampled Data will be saved to: ", output_file))
+    print("Adding Reduction to Seurat Object")
+    pca_dir <- file.path(data_dir, "analysis", "pca", "gene_expression_10_components")
+    umap_file <- file.path(data_dir, "analysis", "umap", "gene_expression_2_components", "projection.csv")
     
-    seurat_obj <- subset(seurat_obj, downsample = 100)
+    if(dir.exists(pca_dir)){
 
-    if( file.exists(output_file) ){
-        # output_file <- file.path(data_dir, paste0(sample_name, "_seurat_downsampled_", as.integer(Sys.time()), ".RDS"))
-        output_file <- paste0(sample_name, "_seurat_downsampled_", as.integer(Sys.time()), ".RDS")
+        print(paste("Adding PCA to the Seurat Object from the following directory: ", pca_dir))        
+        embedding_path <- file.path(pca_dir, "projection.csv")
+        loadings_path <- file.path(pca_dir, "components.csv")
+
+        embeddings <- fread(embedding_path, data.table = FALSE)
+        colnames(embeddings) <- gsub(pattern = "-", replacement = "_", x = colnames(embeddings))
+        row.names(embeddings) <- embeddings$Barcode
+        embeddings <- as.matrix(embeddings[, 2:ncol(embeddings)])
+
+        loadings <- fread(loadings_path, data.table = FALSE)
+        loadings$PC <- paste0("PC_", loadings$PC)
+        row.names(loadings) <- loadings$PC
+        loadings <- t(as.matrix(loadings[, 2:ncol(loadings)]))
+
+        pca <- CreateDimReducObject(embeddings = embeddings, loadings = loadings, assay = "Xenium", key = "PC_")
+
+        seurat_obj@reductions$xenium_pca <- pca
+    }
+    
+    if(file.exists(umap_file)){
+
+        print(paste("Adding UMAP to Seurat Object from the following file: ", umap_file))
+        umap_dat <- fread(umap_file, data.table=FALSE)
+        colnames(umap_dat) <- gsub(pattern = "-", replacement = "_", x = colnames(umap_dat))
+        row.names(umap_dat) <- umap_dat[["Barcode"]]
+        umap_dat <- as.matrix(umap_dat[, 2:ncol(umap_dat)])
+
+        umap <- CreateDimReducObject(embeddings = umap_dat, key = "UMAP_", assay = "Xenium")
+        seurat_obj@reductions$xenium_umap <- umap
     }
 
-    saveRDS(seurat_obj, file = "seurat_object_downsampled.RDS")
+    return(seurat_obj)
+
+}
+
+create_seurat_xenium <- function(data_dir, sample_name = "Xenium_Sample", downsample = TRUE){
+
+    print(paste("Creating Seurat object from Xenium data in: ", data_dir))
+    print(paste("Sample name set to: ", sample_name))
+
+    seurat_obj <- LoadXenium(data.dir = data_dir, fov = "fov", segmentations = "cell")
+    seurat_obj[["cell_id"]] <- rownames(seurat_obj[[]])
+    seurat_obj$orig.ident <- sample_name
+
+    seurat_obj <- add_meta_and_clusters(seurat_obj, data_dir)
+    seurat_obj <- add_reductions(seurat_obj, data_dir)
+
+    if(downsample){
+        print("Saving Downsampled Seurat Object")
+        saveRDS(subset(seurat_obj, downsample = 100), file = "seurat_object_downsampled.RDS")
+    }
+
+    print("Saving Full Seurat Object")
+    saveRDS(seurat_obj, file = "seurat_object.RDS")
 }
 
 main <- function(){
@@ -88,10 +116,6 @@ main <- function(){
     args <- arguments()
     
     seurat <- create_seurat_xenium(data_dir = args$data_dir, sample_name = args$sample_name)
-
-    if(args$downsample){
-        seurat <- downsample_seurat_xenium(seurat_obj = seurat, data_dir = args$data_dir, sample_name = args$sample_name)
-    }
 }
 
 main()
