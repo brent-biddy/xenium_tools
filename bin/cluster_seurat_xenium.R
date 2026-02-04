495f8dc47f0de8343c08cfb5186a6694bfce0810
Brent <255204883+brent-biddy@users.noreply.github.com>
2026-02-04 15:29:44 -0600

    Update globals.maxSize

--- bin/cluster_seurat_xenium.R
+++ bin/cluster_seurat_xenium.R
@@ -41,11 +41,11 @@ main <- function(){
     args <- arguments()
 
     if(args$executor == 'local'){
-        options(future.globals.maxSize = 10e9)
+        options(future.globals.maxSize = 20e9)
         plan(multisession)
         print(plan())
     } else if (args$executor == 'slurm') {
-        options(future.globals.maxSize = 10e9)
+        options(future.globals.maxSize = 20e9)
         plan(multicore)
         print(plan())
     }
